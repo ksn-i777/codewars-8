459f97998ceee9e7b84efd2b694dbd6c4b3bf437
ksn-i777 <104304094+ksn-i777@users.noreply.github.com>
2022-09-20 18:42:28 +0300

    Create 016-Counting sheep.js

---
+++ 016-Counting sheep.js
@@ -0,0 +1,9 @@
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  let sum = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {    
+    if (arrayOfSheep[i] == true) {
+      sum += 1;
+    } 
+  } return sum;
+}
