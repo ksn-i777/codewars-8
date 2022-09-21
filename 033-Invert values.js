2c8af2910d9f51ce9ce146f16e96040010b0c517
ksn-i777 <104304094+ksn-i777@users.noreply.github.com>
2022-09-21 16:20:22 +0300

    Create 033-Invert values.js

---
+++ 033-Invert values.js
@@ -0,0 +1,12 @@
+function invert(array) {
+  let arr = [];
+  if (array.length < 1) {
+    return [];
+  } else {
+    for (let i=0; i<array.length; i++) {
+      arr.push(-array[i]);
+    }
+  }
+  
+   return arr;
+}
