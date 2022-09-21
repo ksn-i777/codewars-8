af6c0987d1e250c2837bf0dd86541598235c9c24
ksn-i777 <104304094+ksn-i777@users.noreply.github.com>
2022-09-21 16:21:41 +0300

    Create 036-Fake Binary.js

---
+++ 036-Fake Binary.js
@@ -0,0 +1,14 @@
+function fakeBin(x){
+    const arr = x.split('');
+    let arr2 = [];
+    let str = '';
+    for (let i=0; i<arr.length; i++) {
+      if (arr[i] < 5) {
+        arr2.push(0);
+      } else {
+        arr2.push(1);
+      }       
+    }
+    str = arr2.join().replace(/[\s.,%]/g, '');
+    return str;
+  }
